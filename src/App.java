5f94aaadafe14ada6c2165a70956ba1d7eeb597b
yosoyeli <eli.vsa05@gmail.com>
2022-12-06 09:45:27 -0600

    Santas List

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+String list[][] = {{"First Name","Last Name","Naughty/Nice","Street", "Gift"},
+                    {"LoGaN", "may", "Naughty", "7730 Whitemarsh Court", "deOdeRant"},
+                    {"Cecilia",	"bOYer", "Nice", "8686 Elm St.","PUddle"},
+                    {"lorElAi",	"moRsE",  "Naughty", "8529 Birchpond St.", "TOMAto"},
+                    {"rory", "black", "Naughty", "39   Fairfield Ave.", "lamp ShaDe"},
+                    {"Denzel", "SancHez", "961  Hanover Ave.","shOEs"}, 
+                    {"maNUEL", "Lambert", "Nice","698  Arrowhead Rd.", "chArgEr"},
+                    {"Trinity",	"FISHER	Nice", "49 Arlington Avenue", "winDow"},
+                    {"maTthiA", "hayEs", "Nice", "23   Woodside Ave.", "LOTION"},
+                    {"Sophia",	"SToUT", "Naughty",	"7640 Andover Dr.",	"teddY beAr"},
+                    {"keNNedy",	"Dunlap", "63   Cypress Rd.", "pAnts"}, 
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                    {"","","","",""},
+                };
+                
     }
+
+    public static String[] printArray(String[] array) { 
+        for (int i = 0; i < array.length; i++) { 
+            System.out.println(array[i]); 
+    }
+    return array;
+}
+public static int[] printArray(int[] array) { 
+    for (int i = 0; i < array.length; i++) { 
+        System.out.println(array[i]); 
+    }
+    return array; 
+
+}
+
 }
