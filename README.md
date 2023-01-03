a57ea1105e9fa2a7ee6afc1836c3b62df00b7241
yosoyeli <eli.vsa05@gmail.com>
2023-01-03 09:25:18 -0600

    first

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
-## Getting Started
+# MasseyIsSanta
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+### *Tools used*
+- Language: Java 
+- Compiler: VSCode
+- Version: 1.0
 
-## Folder Structure
+1. An array is printed displaying Santa's naughty and nice list. Naughty children are printed in red, nice children in green, and children with an unknown status are printed in yellow. 
+2. The user (presumably an elf) is prompted with two options.
+    1. By typing '1' the user can add a new child's information to the list.
+        1. The user is prompted to individually input the child's first and last name, naughty or nice status, 
+            address, and the gift they would like to recieve. 
+        2. Once this information is taken in it is stored into the array. Naughty children automatically recieve coal. 
+    2. By typing '2' the user can search for a child that's already inside of the database. 
+    3. 
 
-The workspace contains two folders by default, where:
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
 
-## Dependency Management
 
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+3. Players choose rock, paper, or scissors. 
+   1. Represent Choices with a `char`
+        1. R - rock 
+        2. P - paper 
+        3. S - scissors
+   2. Store the players' choices. 
+        1. Prompt `Scanner` object `nextChar()`
+        2. `char playerOneChoice`
+        3. `char playerTwoChoice`
+4. Print count down rock paper scissors shoot. Then reveal choices on shoot. 
+   1. `Thread.sleep()` to artificially wait
+5. Compare choices.
+   1. Scissors beats paper.
+   2. Rock beats scissors.
+   3. Paper beats rock. 
--- src/App.java
+++ src/App.java
@@ -1,21 +1,47 @@
-
+import java.util.Scanner;
 
 public class App {
-
+    
     public static void main(String[] args) throws Exception {
         
+        Scanner in = new Scanner(System.in);
 
-String list[][] = {{"First Name","Last Name","Naughty/Nice","Street", "Gift"},
+        // Elements 
+
+        int userIn; 
+        int userChoice; 
+       
+        String childFirstName1; 
+        String childLastName1; 
+        String childStatus1; 
+        String childAddress1; 
+        String childGift1; 
+        String childFirstName2; 
+        String childLastName2; 
+        String childStatus2; 
+        String childAddress2; 
+        String childGift2; 
+        String childFirstName3; 
+        String childLastName3; 
+        String childStatus3; 
+        String childAddress3; 
+        String childGift3; 
+        
+        // Naughty & Nice List Array 
+
+        String santasList[][] = {
+
+                    {"First Name","Last Name","Naughty/Nice","Street", "Gift"},
                     {"LoGaN", "may", "Naughty", "7730 Whitemarsh Court", "deOdeRant"},
                     {"Cecilia",	"bOYer", "Nice", "8686 Elm St.","PUddle"},
                     {"lorElAi",	"moRsE",  "Naughty", "8529 Birchpond St.", "TOMAto"},
                     {"rory", "black", "Naughty", "39   Fairfield Ave.", "lamp ShaDe"},
-                    {"Denzel", "SancHez", "961  Hanover Ave.","shOEs"}, 
+                    {"Denzel", "SancHez", "", "961  Hanover Ave.","shOEs"}, 
                     {"maNUEL", "Lambert", "Nice","698  Arrowhead Rd.", "chArgEr"},
-                    {"Trinity",	"FISHER	Nice", "49 Arlington Avenue", "winDow"},
+                    {"Trinity",	"FISHER", "Nice", "49 Arlington Avenue", "winDow"},
                     {"maTthiA", "hayEs", "Nice", "23   Woodside Ave.", "LOTION"},
                     {"Sophia",	"SToUT", "Naughty",	"7640 Andover Dr.",	"teddY beAr"},
-                    {"keNNedy",	"Dunlap", "63   Cypress Rd.", "pAnts"}, 
+                    {"keNNedy",	"Dunlap", "", "63   Cypress Rd.", "pAnts"}, 
                     {"","","","",""},
                     {"","","","",""},
                     {"","","","",""},
@@ -25,23 +51,158 @@ String list[][] = {{"First Name","Last Name","Naughty/Nice","Street", "Gift"},
                     {"","","","",""},
                     {"","","","",""},
                     {"","","","",""},
-                    {"","","","",""},
-                };
+                    {"","","","",""},     
+            };
+            
+                printArray(santasList);
+
+                for (int row = 0; row < santasList.length; row++) { 
+                    
+                    for (int column = 0; column < santasList[row].length; column++) { 
+                        santasList[row][column] = santasList[row][column].toUpperCase();
+                    }
+
+                }
+
                 
+           
+        // Loops to add new children to the Array List or to Search for kids that are already on the list
+
+        // Inital Prompt 
+
+        do { 
+            System.out.println(Colors.ANSI_BLUE + "Would you like to add another child to this list?");
+                Thread.sleep(500);
+            System.out.println("Or view the list?");
+                Thread.sleep(500);
+            System.out.println("Type '1' to add a child.");
+            System.out.println("Type '2' to view the existing list.");
+                
+            userIn = in.nextInt();
+            userChoice = 0;
+            userChoice = userIn;
+            
+
+
+        // Inner Loop w/results    
+
+        do { 
+
+            // Error Handling 
+
+            if (userChoice < 1 || userChoice > 2) { 
+                System.out.println("Invalid input. Try again."); 
+
+                System.out.println("Type '1' to add a child.");
+
+                System.out.println("Type '2' to view the existing list.");
+
+                in.nextInt();
+            }
+
+             // Search for a child on Santa's list 
+
+            else if (userChoice == 2) {
+                
+            System.out.println("Okay! Initializing Santa's list now...");
+            Thread.sleep(1000);
+            
+            printArray(santasList);
+
+            for (int row = 0; row < santasList.length; row++) { 
+                
+                for (int column = 0; column < santasList[row].length; column++) { 
+                    santasList[row][column] = santasList[row][column].toUpperCase();
+                    
+                }
+
+            }
+
+
+            }
+            
+            // Add a new child to the list 
+
+            else if (userChoice == 1) { 
+
+                 // Recieving user input and storing the child's information 
+
+                System.out.println("Okay! Let's get started..." + Colors.ANSI_RESET); 
+                Thread.sleep(500);
+
+                System.out.println(Colors.ANSI_PURPLE + "Input the child's first name");
+                in.nextLine();
+                childFirstName1 = in.nextLine();
+                
+                System.out.println("First Name: " + childFirstName1 + Colors.ANSI_RESET);
+                Thread.sleep(500); 
+                System.out.println(Colors.ANSI_BLACK + "Input the child's last name"); 
+                childLastName1 = in.nextLine();
+                
+                System.out.println("Last Name: " + childLastName1 + Colors.ANSI_RESET);
+                Thread.sleep(500); 
+                System.out.println(Colors.ANSI_CYAN + "Input the child's naughty or nice status"); 
+                childStatus1 = in.nextLine();
+                
+                System.out.println("Status: " + childStatus1 + Colors.ANSI_RESET);
+                Thread.sleep(500); 
+                System.out.println(Colors.ANSI_YELLOW + "Input the child's address"); 
+                childAddress1 = in.nextLine();
+                
+                System.out.println("Address: " + childAddress1 + Colors.ANSI_RESET);
+                Thread.sleep(500);  
+                System.out.println(Colors.ANSI_RED + "Input the child's gift"); 
+                childGift1 = in.nextLine();
+                System.out.println("Gift: " + childGift1 + Colors.ANSI_RESET);
+                Thread.sleep(500); 
+
+                System.out.println(Colors.ANSI_GREEN + "All done! " + childFirstName1 + " has been added to Santa's list!");
+                
+                Thread.sleep(2000); 
+                
+                System.out.println("Would you like to view Santas List, Search for a child on the List, add another child, or exit?" + Colors.ANSI_RESET);
+                Thread.sleep(2000);
+                
+
+                in.nextInt();
+            } 
+   
+         while(true);
+            }  while(true);
+          
+
+        
+
+            
+        } while(true); 
     }
 
-    public static String[] printArray(String[] array) { 
-        for (int i = 0; i < array.length; i++) { 
-            System.out.println(array[i]); 
-    }
-    return array;
-}
-public static int[] printArray(int[] array) { 
-    for (int i = 0; i < array.length; i++) { 
-        System.out.println(array[i]); 
-    }
-    return array; 
 
+    // Printing Santa's List 
+
+    public static void printArray(String[][] array) { 
+        for(int row = 0; row < array.length; row++)  { 
+            if (array[row][2].toUpperCase().equals("NAUGHTY")) { 
+                System.out.println(Colors.ANSI_RED); 
+            }
+            else if(array[row][2].toUpperCase().equals("NICE")) {
+                System.out.println(Colors.ANSI_GREEN); 
+            }
+            else { 
+                System.out.println(Colors.ANSI_YELLOW); 
+            }
+            if (array[row][2].toUpperCase().equals("NAUGHTY")) { 
+                array[row][4] = ("COAL");
+            }
+            for(int column = 0; column < array[row].length; column++) { 
+                System.out.println(array[row][column].toUpperCase() ); 
+            }
+                System.out.println(Colors.ANSI_RESET);
+        
+        }
+      
+} 
+   
 }
 
-}
+
